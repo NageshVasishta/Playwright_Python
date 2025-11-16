5ccfa72c933509af88328431aadc62e7464233d4
NageshVasishta <nageshrangarao@gmail.com>
2025-11-16 19:34:15 +0530

    initial commit

---
+++ test_1.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import Playwright
+
+
+def test_1(playwright : Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.google.com")
+
